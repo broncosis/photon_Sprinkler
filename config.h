5cc694a4be34ee28dba29db4feeb7697fb374d65
Broncosis <rob@bobcat.ca>
2017-08-05 09:50:19 -0700

    modified:   config.h

--- config.h
+++ config.h
@@ -3,4 +3,21 @@
 #define THINGSPEAKCHANNEL2  //second thing speak chanel id here
 #define THINGSPEAKWRITEAPI2  "second thing speak write api here"
 
-//more config stuff here soon
+
+
+#define ZONE1_MONITOR 0 // 1
+#define ZONE2_MONITOR 0 // 2
+#define ZONE3_MONITOR 0 // 4
+#define ZONE4_MONITOR 0 // 3
+#define ZONE5_MONITOR 0
+#define TIME_ZONE -8
+#define PIN1  0B00000001  // binary value of the first pin
+#define PIN2  0B00000010  // binary value of the second pin
+#define PIN3  0B00000100  // third pin
+#define PIN4  0B00001000  // fourth pin
+#define PIN5  0B00010000  // fifth pin
+#define PIN6  0B00100000  // sixth pin
+#define PIN7  0B01000000  // seventh pin
+#define PIN8  0B10000000  // eighth pin
+#define Addr 0x20         // IO expander address
+//ultrasonic sensor config
